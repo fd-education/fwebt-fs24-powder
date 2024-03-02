52603a37484b53a76d66e74b4436bad3522155c9
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-02 11:48:22 +0000

    feature(playername): implement state for playername, navigate user from root page according to that state

--- workspaces/client/src/pages/Root.tsx
+++ workspaces/client/src/pages/Root.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { usePlayerNameStore } from '../domain/state/playerNameStore';
 
 export const RootPage = () => {
+  const {playerName} =  usePlayerNameStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(playerName){
+      navigate('/lobby');
+    } else {
+      navigate('/landing');
+    }
+  },[])
+
   return (
     <>
-      <h1>This is the root page</h1>
       <Outlet />
     </>
   );
