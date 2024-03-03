6e76f48d94a9b5f6f5a27e4e52b9b0f973eaf3c1
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-03 17:14:08 +0000

    feature(game): add game logic at game page

--- workspaces/client/src/pages/Game.tsx
+++ workspaces/client/src/pages/Game.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { TitleSmall } from '../components/util/TitleSmall';
-import { Board } from '../components/game/Board';
+import { Board } from '../components/game/board/Board';
 import { Score } from '../components/game/Score';
 import { Preview } from '../components/game/Preview';
 import { SettingsGroup } from '../components/settings/SettingsGroup';
+import { useGame } from '../hooks/useGame';
+import { PowderButton } from '../components/util/PowderButton';
 
 export const GamePage = () => {
-    return (
-      <div className='h-full w-full flex justify-between'>
-        <div className='h-full flex flex-col justify-center'>
-          <TitleSmall />
-          <Score />
-        </div>
-        <div className='h-full flex flex-col justify-center items-center'>
-          <Board />
-          <SettingsGroup/>
-        </div>
-        <div className='h-full flex flex-col justify-center'>
-          <Preview />
-        </div>
+  const {board, start, gameStarted} = useGame();
+
+  return (
+    <div className='h-full w-full flex justify-between'>
+      <div className='h-full flex flex-col justify-center'>
+        <TitleSmall />
+        <Score />
       </div>
-    );
+      <div className='h-full flex flex-col justify-center items-center'>
+        <Board currentState={board}/>
+        {!gameStarted && <PowderButton text='start' clickHandler={start}/>}
+        <SettingsGroup />
+      </div>
+      <div className='h-full flex flex-col justify-center'>
+        <Preview />
+      </div>
+    </div>
+  );
 };
