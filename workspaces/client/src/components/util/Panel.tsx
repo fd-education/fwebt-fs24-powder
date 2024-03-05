6e1ef692f4e740a962093379c6ead709a499e6ea
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-05 11:02:00 +0000

    feature(panels): make panels height and width controllable

--- workspaces/client/src/components/util/Panel.tsx
+++ workspaces/client/src/components/util/Panel.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BaseProps } from '../../domain/types/BaseProps';
 
-export const Panel = (props: BaseProps) => {
+interface PanelProps extends BaseProps{
+  height?: string;
+  width?: string;
+}
+
+export const Panel = ({height = 'fit', width = 'fit', children}: PanelProps) => {
   return (
     <div
-      className={`h-fit w-fit py-8 px-16 bg-primary-light dark:bg-primary-dark border-4 border-primary-dark dark:border-primary-light flex flex-col items-center shadow shadow-primary-dark dark:shadow-primary-light`}
+      className={`h-[${height}] w-[${width}] py-8 px-16 bg-primary-light dark:bg-primary-dark border-4 border-primary-dark dark:border-primary-light flex flex-col items-center shadow shadow-primary-dark dark:shadow-primary-light`}
     >
-      {props.children}
+      {children}
     </div>
   );
 };
