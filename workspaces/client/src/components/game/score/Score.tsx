a8b03f4f6b973d89e364f3274e1d3151e8ea6380
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-05 14:19:39 +0000

    feature(score): style score component

---
+++ workspaces/client/src/components/game/score/Score.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Panel } from '../../util/Panel';
+import { PanelHeading } from '../../util/PanelHeading';
+import { PowderButton } from '../../util/PowderButton';
+import { useGame } from '../../../hooks/useGame';
+import { NumberDisplay } from './NumberDisplay';
+
+interface ScoreProps {
+  score: number;
+  lines: number;
+}
+
+export const Score = ({ score, lines }: ScoreProps) => {
+  const { pauseGame, continueGame, endGame } = useGame();
+
+  return (
+    <Panel>
+      <div className='flex flex-col justify-center space-y-4'>
+        <div>
+          <PanelHeading text='Score' />
+          <NumberDisplay number={score} />
+        </div>
+        <div>
+          <PanelHeading text='Lines' />
+          <NumberDisplay number={lines} />
+        </div>
+        <PowderButton text='pause' clickHandler={() => pauseGame()} />
+        <PowderButton text='end' clickHandler={endGame} />
+      </div>
+    </Panel>
+  );
+};
