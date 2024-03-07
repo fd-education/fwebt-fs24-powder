3d29172cdb56a2d3ad539db00a06bd387ad9e4cd
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-07 18:17:34 +0100

    fix: stabilize screenmode setting

--- workspaces/client/src/pages/Root.tsx
+++ workspaces/client/src/pages/Root.tsx
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { usePlayerNameStore } from '../domain/state/playerNameStore';
+import { useScreenModeStore } from '../domain/state/screenModeStore';
+import { ScreenMode } from '../domain/enums/ScreenMode.enum';
 
 export const RootPage = () => {
   const { playerName } = usePlayerNameStore();
+  const { screenMode } = useScreenModeStore();
   const navigate = useNavigate();
 
   useEffect(() => {
+    if (screenMode === ScreenMode.DARK) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+
     if (playerName) {
       navigate('/lobby');
     } else {
