26cdeb8fd03d14ef645188a3fbd224d932010bdf
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-10 20:13:14 +0100

    feature(panels): parametrise panel transparency

--- workspaces/client/src/components/util/Panel.tsx
+++ workspaces/client/src/components/util/Panel.tsx
@@ -4,16 +4,18 @@ import { BaseProps } from '../../domain/types/BaseProps';
 interface PanelProps extends BaseProps {
   height?: string;
   width?: string;
+  transparent?: boolean;
 }
 
 export const Panel = ({
   height = 'h-fit',
   width = 'h-fit',
+  transparent = true,
   children,
 }: PanelProps) => {
   return (
     <div
-      className={`${height} ${width} py-8 px-16 bg-primary-light-trans8 dark:bg-primary-dark-trans8 border-4 border-primary-dark dark:border-primary-light flex flex-col items-center shadow shadow-primary-dark dark:shadow-primary-light`}
+      className={`${height} ${width} py-8 px-16 ${transparent ? 'bg-primary-light-trans8 dark:bg-primary-dark-trans8' : 'bg-primary-light dark:bg-primary-dark'}  border-4 border-primary-dark dark:border-primary-light flex flex-col items-center shadow shadow-primary-dark dark:shadow-primary-light`}
     >
       {children}
     </div>
