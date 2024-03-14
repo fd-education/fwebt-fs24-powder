821925e2be977d8daf5557eb68cae396bbb8c0aa
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-14 12:29:27 +0000

    feature(scoreboard-api): create hook for scoreboard api

---
+++ workspaces/client/src/hooks/useScoreboardAPI.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { powderConfig } from '../domain/config/PowderConfig';
+import { usePlayerNameStore } from '../domain/state/playerNameStore';
+import { ScoreboardResponse } from '@powder/common';
+
+export const useScoreboardApi = () => {
+  const [loading, setLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [error, setError] = useState('');
+  const [data, setData] = useState<ScoreboardResponse>();
+
+  const { playerName } = usePlayerNameStore();
+  const { SERVER_URL, SCOREBOARD_ENDPOINT } = powderConfig;
+
+  useEffect(() => {
+    const url = `${SERVER_URL}/${SCOREBOARD_ENDPOINT}?name=${playerName}`;
+    fetch(url, {
+      method: 'GET',
+    })
+      .then((response) => {
+        if(!response.ok){
+          throw new Error(`${response.status} - ${response.statusText}`);
+        }
+
+        return response.json()
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(e => {
+        setHasError(true);
+        setError(e);
+      });
+  }, []);
+
+  return {
+    loading,
+    data,
+    hasError,
+    error
+  };
+}
