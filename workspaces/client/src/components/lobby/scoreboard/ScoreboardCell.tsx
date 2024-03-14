d73eea75d67ecae9837dbf195b9a671ca978cdda
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-14 13:42:56 +0000

    feature(playername): reduce size of scoreboard entries

--- workspaces/client/src/components/lobby/scoreboard/ScoreboardCell.tsx
+++ workspaces/client/src/components/lobby/scoreboard/ScoreboardCell.tsx
@@ -7,9 +7,7 @@ interface ScoreboardCellProps {
 export const ScoreboardCell = ({ text }: ScoreboardCellProps) => {
   return (
     <td className='py-1 dark:text-primary-light text-primary-dark'>
-      <p className='text-lg truncate text-center text-ellipsis max-w-56'>
-        {text}
-      </p>
+      <p className='truncate text-center text-ellipsis max-w-56'>{text}</p>
     </td>
   );
 };
