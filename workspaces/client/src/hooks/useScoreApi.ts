25a78f91697c826c32d9bdcbb3e1030520f4d8c4
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-15 14:46:36 +0100

    chore: run format

--- workspaces/client/src/hooks/useScoreApi.ts
+++ workspaces/client/src/hooks/useScoreApi.ts
@@ -1,5 +1,5 @@
 import { ScoreRequest, ScoreResponse } from '@powder/common';
-import { usePlayerNameStore } from '../domain/state/playerNameStore'
+import { usePlayerNameStore } from '../domain/state/playerNameStore';
 import { useScoreStore } from '../domain/state/scoreStore';
 import { powderConfig } from '../domain/config/PowderConfig';
 
@@ -8,37 +8,37 @@ export const useScoreApi = () => {
   const { playerScore } = useScoreStore();
   const { SERVER_URL, SCORE_ENDPOINT } = powderConfig;
 
-  const saveScore = () =>{
-    if(playerScore === 0) return;
+  const saveScore = () => {
+    if (playerScore === 0) return;
 
     const payload: ScoreRequest = {
       name: playerName,
       score: playerScore,
       timestamp: new Date().toISOString(),
-    }
+    };
     const url = `${SERVER_URL}/${SCORE_ENDPOINT}`;
 
     postRequest<ScoreRequest, ScoreResponse>(url, payload);
-  }
+  };
 
   const postRequest = async <T, P>(url: string, payload: T): Promise<P> => {
-    try{
+    try {
       const response = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(payload),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+          'Content-Type': 'application/json',
+        },
+      });
 
       console.log(response);
       return response as P;
-    } catch(e){
+    } catch (e) {
       console.error(e);
     }
-  }
+  };
 
   return {
     saveScore,
   };
-}
+};
