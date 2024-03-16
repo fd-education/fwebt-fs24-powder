7244f2fa577eb1b79d7882b584dd0bf5ed06c9b3
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-16 23:37:07 +0100

    test(game-options): test game options interface and behaviour

--- workspaces/client/src/components/lobby/GameOptions.tsx
+++ workspaces/client/src/components/lobby/GameOptions.tsx
@@ -7,32 +7,28 @@ import { useNavigate } from 'react-router-dom';
 export const GameOptions = () => {
   const navigate = useNavigate();
 
-  const playSinglePlayer = () => {
-    navigate('/game');
-  };
-
-  const playLocalMultiplayer = () => {
-    console.warn('Local multiplayer to be implemented');
-    navigate('/game');
-  };
-
-  const playRemoteMultiplayer = () => {
-    console.warn('Remote multiplayer to be implemented');
-    navigate('/game');
-  };
-
   return (
     <Panel>
       <div className='space-y-8'>
         <div className='flex flex-col items-center space-y-2'>
           <PanelHeading text='Singleplayer' />
-          <PowderButton text='play' clickHandler={playSinglePlayer} />
+          <PowderButton text='play' clickHandler={() => navigate('/game')} />
         </div>
         <div className='flex flex-col items-center space-y-2'>
           <PanelHeading text='Multiplayer' />
           <div className='space-x-6'>
-            <PowderButton text='local' clickHandler={playLocalMultiplayer} />
-            <PowderButton text='remote' clickHandler={playRemoteMultiplayer} />
+            <PowderButton
+              text='local'
+              clickHandler={() =>
+                console.warn('Local multiplayer to be implemented')
+              }
+            />
+            <PowderButton
+              text='remote'
+              clickHandler={() =>
+                console.warn('Remote multiplayer to be implemented')
+              }
+            />
           </div>
         </div>
       </div>
