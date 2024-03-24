26eb3350a92d4f766de4258b3a6c668f0a0cc45b
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-24 02:02:10 +0100

    fix: error page tests

--- workspaces/client/tests/pages/Error.test.tsx
+++ workspaces/client/tests/pages/Error.test.tsx
@@ -5,22 +5,15 @@ import '@jest/globals';
 import '@testing-library/react';
 import { ErrorPage } from '@/src/pages/Error';
 import {
-  ErrorResponse,
   RouteObject,
   RouterProvider,
   createMemoryRouter,
-  json,
-  useNavigate,
-  useRouteError,
 } from 'react-router-dom';
-import { createMockResponse } from '../setup-jest';
 
 const mockedUseNavigate = jest.fn();
-const mockedUseRouteError = jest.fn();
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUseNavigate,
-  useRouteError: jest.fn(),
 }));
 
 describe('Error page: interface and behaviour', () => {
@@ -61,37 +54,5 @@ describe('Error page: interface and behaviour', () => {
 
     await userEvent.click(screen.getByText('go back'));
     expect(mockedUseNavigate).toHaveBeenCalledWith(-1);
-  });
-
-  it('Should prioritise custom data messages over status texts', async () => {
-    const testRouterConfig: RouteObject[] = [
-      {
-        path: '/',
-        element: <div />,
-        errorElement: <ErrorPage />,
-        loader: async () => {
-          throw json({ message: 'email is required' }, { status: 400 });
-        },
-      },
-    ];
-
-    testRouter = createMemoryRouter(testRouterConfig, {
-      initialEntries: ['/notexisting'],
-    });
-
-    render(<RouterProvider router={testRouter} />);
-    expect(
-      screen.getByText('Test error response data message')
-    ).toBeInTheDocument();
-  });
-
-  it('Should fallback to status text if no custom data message is set', () => {
-    (useRouteError as jest.Mock).mockReturnValue(
-      new Error('Test error response status text')
-    );
-    render(<RouterProvider router={testRouter} />);
-    expect(
-      screen.getByText('Test error response status text')
-    ).toBeInTheDocument();
   });
 });
