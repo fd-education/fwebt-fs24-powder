c86adcc402553430bffa0cb7918793f6ba79001b
Fabian Diemand <fabian.diemand@hotmail.com>
2024-03-24 01:05:39 +0100

    fix: implement new new Panel width

--- workspaces/client/src/components/game/Pause.tsx
+++ workspaces/client/src/components/game/Pause.tsx
@@ -9,7 +9,7 @@ export const Pause = () => {
 
   return (
     <div className='h-full w-full fixed top-0 left-0  bg-primary-light-trans7 dark:bg-primary-dark-trans7 flex items-center justify-center'>
-      <Panel transparent={false}>
+      <Panel transparent={false} width='w-fit'>
         <div className='flex flex-col items-center space-y-8'>
           <PanelHeading text='paused' />
           <PowderButton text='continue' clickHandler={() => continueGame()} />
