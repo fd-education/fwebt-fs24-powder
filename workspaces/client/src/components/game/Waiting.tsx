f5e7c5847cb6ef819e8a43dadd8b60caee27f57e
Fabian Diemand <fabian.diemand@hotmail.com>
2024-04-30 22:04:30 +0000

    feature(waiting-info): add background on waiting info

--- workspaces/client/src/components/game/Waiting.tsx
+++ workspaces/client/src/components/game/Waiting.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Panel } from '../util/Panel';
 import { PanelHeading } from '../util/PanelHeading';
 import { useTranslation } from 'react-i18next';
+import { PopupUnderlay } from './PopupUnderlay';
 
 export const Waiting = () => {
   const { t } = useTranslation();
 
   return (
+    <>
+    <PopupUnderlay/>
     <div className='w-fit h-fit absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex items-center justify-center z-20'>
       <Panel transparent={false} width='w-fit'>
         <div className='flex flex-col items-center space-y-8'>
@@ -18,5 +21,6 @@ export const Waiting = () => {
         </div>
       </Panel>
     </div>
+    </>
   );
 };
